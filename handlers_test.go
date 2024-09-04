5b90b5b6406bbf72428cbfc11b28cd1179a6ffa1
Kevin Mollenhauer <mollen.kev@gmail.com>
2024-09-04 10:15:55 +0200

    add: better test on pub IP handler

--- handlers_test.go
+++ handlers_test.go
@@ -6,6 +6,19 @@ import (
   "testing"
 )
 
+// Struct representing our tests cases
+var ipTests = []struct {
+  addr string // input
+  expected int // expected error
+}{
+  {"127.0.0.1:1234", 200},
+  {"2001:0db8:85a3:0000:0000:8a2e:0370:7334", 400},
+  {"[3fff::]:1234", 200},
+  {"", 400},
+  {":123", 400},
+  {"string", 400},
+}
+
 func TestHealthCheckHandler(t *testing.T) {
   req,err := http.NewRequest("GET", "/healthz", nil)
 
@@ -41,11 +54,16 @@ func TestIPHandler(t *testing.T) {
 
   rr := httptest.NewRecorder()
   handler := http.HandlerFunc(GetPubIPHandler)
-
-  handler.ServeHTTP(rr, req)
-
-  if status := rr.Code; status != http.StatusBadRequest {
-    t.Errorf("handler returned wrong status code: got %v want %v",
-      status, http.StatusOK)
-  }
+  // Loop over test cases
+  for _, ipt := range(ipTests) {
+    // Overwrite remote addr with test cases addr
+    req.RemoteAddr = ipt.addr
+    rr = httptest.NewRecorder()
+    handler.ServeHTTP(rr, req)
+    
+    if status := rr.Code; status != ipt.expected {
+      t.Errorf("handler returned wrong status code: got %v want %v",
+        status, ipt.expected)
+    }
+  } 
 }
